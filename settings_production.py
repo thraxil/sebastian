7344614960ae71788c24915ba0f1906182c473e5
Anders Pearson <anders@columbia.edu>
2010-07-20 11:44:00 -0400

    allow local settings in production

--- settings_production.py
+++ settings_production.py
@@ -14,3 +14,10 @@ TEMPLATE_DIRS = (
 MEDIA_ROOT = '/var/www/tmp/sebastian/media/data/'
 
 SERVER_EMAIL = 'anders@columbia.edu'
+
+# local_settings.py can be used to override environment-specific settings
+# like database and email that differ between development and production.
+try:
+    from local_settings import *
+except ImportError:
+    pass
