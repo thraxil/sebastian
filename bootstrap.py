778334e898b3c9d148c97db37d45cedb724621b1
Anders Pearson <anders@columbia.edu>
2013-07-06 18:32:55 -0400

    pypi mirror

--- bootstrap.py
+++ bootstrap.py
@@ -10,8 +10,22 @@ vedir = os.path.join(pwd,"ve")
 if os.path.exists(vedir):
     shutil.rmtree(vedir)
 
-subprocess.call(["python",os.path.join(pwd,"pip.py"),"install",
-                 "-E",os.path.join(pwd,"ve"),
-                 "--enable-site-packages",
-                 "--requirement",os.path.join(pwd,"requirements/apps.txt")])
+virtualenv_support_dir = os.path.abspath(
+    os.path.join(
+        pwd, "requirements", "virtualenv_support"))
 
+ret = subprocess.call(["python", "virtualenv.py",
+                       "--extra-search-dir=%s" % virtualenv_support_dir,
+                       "--never-download",
+                       vedir])
+if ret:
+    exit(ret)
+
+ret = subprocess.call(
+    [os.path.join(vedir, 'bin', 'pip'), "install",
+     "-E", vedir,
+     "--index-url=http://pypi.ccnmtl.columbia.edu/",
+     "--requirement",
+     os.path.join(pwd, "requirements.txt")])
+if ret:
+    exit(ret)
