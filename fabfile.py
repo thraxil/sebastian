d98cf96f0431d78012b47d1450cc63845a4ff089
Anders Pearson <anders@columbia.edu>
2014-03-05 21:30:25 -0500

    deploy to orlando

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 from fabric.api import run, sudo, local, cd, env
 
-env.hosts = ['maru.thraxil.org']
+env.hosts = ['orlando.thraxil.org']
 env.user = 'anders'
 nginx_hosts = ['lolrus.thraxil.org']
 
