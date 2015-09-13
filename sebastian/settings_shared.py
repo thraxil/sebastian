3564080d13ea16d8f52991590edff272c6246e9c
Anders Pearson <anders@columbia.edu>
2015-09-13 17:30:45 +0200

    convert to thraxilsettings

--- sebastian/settings_shared.py
+++ sebastian/settings_shared.py
@@ -1,152 +1,16 @@
 # flake8: noqa
-import sys
-import os
+import os.path
+from thraxilsettings.shared import common
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+app = 'sebastian'
+base = os.path.dirname(__file__)
+locals().update(common(app=app, base=base))
 
-ADMINS = (
-    ('Anders Pearson', 'anders@columbia.edu'),
-)
-
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sebastian',
-        'HOST': '',
-        'PORT': 5432,
-        'USER': '',
-        'PASSWORD': '',
-    }
-}
-
-if 'test' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-            'HOST': '',
-            'PORT': '',
-            'USER': '',
-            'PASSWORD': '', }}
-
-SOUTH_TESTS_MIGRATE = False
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/New_York'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = False
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/var/tmp/sebastian/'
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/uploads/'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'v1_xm((z)ni$)nx#2496p(a*n^b9((pf0kxum+tj#o@c29pd@3'
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.request',
-    'django.core.context_processors.static',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-)
-
-ROOT_URLCONF = 'sebastian.urls'
-
-TEMPLATE_DIRS = (
-    "/home/anders/code/python/sebastian/leitner/templates/",
-)
-
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
+INSTALLED_APPS += [  # noqa
     'sebastian.leitner',
-    'django.contrib.admin',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_statsd',
-    'django_markwhat',
-    'gunicorn',
-    'compressor',
-    'smoketest',
 ]
 
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_HTTPONLY = True
-
-STATSD_CLIENT = 'statsd.client'
-STATSD_PREFIX = 'sebastian'
-STATSD_HOST = '127.0.0.1'
-STATSD_PORT = 8125
-
-STATIC_URL = "/media/"
-STATICFILES_DIRS = (
-    os.path.abspath(os.path.join(os.path.dirname(__file__), "../media/")),
-)
-STATIC_ROOT = ""
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-
-COMPRESS_URL = "/media/"
-COMPRESS_ROOT = "media/"
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSMinFilter',
-]
-
-THUMBNAIL_SUBDIR = "thumbs"
-
-ALLOWED_HOSTS = ['localhost', 'cards.thraxil.org']
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-}
+ALLOWED_HOSTS += ['cards.thraxil.org']  # noqa
 
 try:
     from local_settings import *
