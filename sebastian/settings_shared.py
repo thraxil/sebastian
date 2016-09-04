467c50cab563c2a85f3e66a998090f0737f0c45d
Anders Pearson <anders@columbia.edu>
2016-09-04 22:56:48 +0100

    :arrow_up: Django 1.10

--- sebastian/settings_shared.py
+++ sebastian/settings_shared.py
@@ -14,6 +14,27 @@ ALLOWED_HOSTS += ['cards.thraxil.org']  # noqa
 
 USE_TZ = False
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(os.path.dirname(__file__), "templates"),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
 try:
     from local_settings import *
 except ImportError:
