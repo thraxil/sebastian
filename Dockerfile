07af94fb298f8c7209dccd1c1502ba9ee18b81d7
Anders Pearson <anders@thraxil.org>
2020-09-26 15:05:28 +0100

    upgrade django base

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM thraxil/django.base:2020-01-20-46ffcccd02f7
+FROM thraxil/django.base:2020-09-26-83678bbd1abe
 COPY requirements.txt /app/requirements.txt
 RUN /ve/bin/pip3 install -r /app/requirements.txt && touch /ve/sentinal
 WORKDIR /app
--- config.mk
+++ config.mk
@@ -5,11 +5,7 @@ FLAKE8 ?= $(VE)/bin/flake8
 SYS_PYTHON ?= python3
 PIP ?= $(VE)/bin/pip3
 SENTINAL ?= $(VE)/sentinal
-WHEEL_VERSION ?= 0.30.0
 REQUIREMENTS ?= requirements.txt
-VIRTUALENV ?= virtualenv.py
-SUPPORT_DIR ?= requirements/virtualenv_support/
-WHEELHOUSE ?= wheelhouse
 
 JS_FILES ?= media/js/
 TAG ?= latest
