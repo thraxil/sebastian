a4d128b28d1668306080c822d8f04f3cf96aa711
Anders Pearson <anders@thraxil.org>
2024-03-24 16:14:55 +0000

    couple simple tests

---
+++ sebastian/leitner/tests/factories.py
@@ -0,0 +1,44 @@
+import factory
+from django.contrib.auth.models import User
+
+from sebastian.leitner.models import Card, Deck, Face, UserCard
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: "user%d" % n)
+    email = factory.Sequence(lambda n: "user%d@example.com" % n)
+
+
+class FaceFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Face
+
+    content = factory.Sequence(lambda n: "Face %d" % n)
+
+
+class DeckFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Deck
+
+    name = factory.Sequence(lambda n: "Deck %d" % n)
+    user = factory.SubFactory(UserFactory)
+
+
+class CardFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Card
+
+    front = factory.SubFactory(FaceFactory)
+    back = factory.SubFactory(FaceFactory)
+    deck = factory.SubFactory(DeckFactory)
+
+
+class UserCardFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = UserCard
+
+    user = factory.SubFactory(UserFactory)
+    card = factory.SubFactory(CardFactory)
---
+++ sebastian/leitner/tests/test_models.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+
+from .factories import CardFactory, DeckFactory, FaceFactory
+
+
+class TestFace(TestCase):
+    def test_face(self):
+        f = FaceFactory()
+        self.assertTrue(133 < f.size() < 140)
+
+
+class TestDeck(TestCase):
+    def test_empty_deck(self):
+        d = DeckFactory()
+        self.assertEqual(d.num_cards(), 0)
+        self.assertEqual(d.num_cards_due(d.user), 0)
+        self.assertEqual(d.num_unlearned(d.user), 0)
+
+
+class TestCard(TestCase):
+    def test_card(self):
+        c = CardFactory()
+        self.assertEqual(c.deck.num_cards(), 1)
